32776c9a02141c5ba8076e765d9760c19e229ffb
sudharshanyadav <sudharshanyadav.iimt@gmail.com>
2022-12-28 13:41:01 +0530

    theme extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'pages/home_page.dart';
+import 'pages/login_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +14,24 @@ void main() {
     Widget build(BuildContext context) {
 
       return  MaterialApp(
-        home:HOmePageApp(),
+
+        themeMode: ThemeMode.light,
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+        ),
+        initialRoute: "/home",
+        routes:  {
+          "/": (context)=> LoginPage(),
+          "/home":(context)=>HomePage(),
+          "/login":(context)=>LoginPage(),
+
+
+        },
 
       );
     }
+
   }
